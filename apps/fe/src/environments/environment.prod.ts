9c96e5eaaa2d7e39bb257d46f1e6312b656277e2
mtrapass <matteo.trapasso97@gmail.com>
2021-05-26 16:47:56 +0200

    ok

--- apps/fe/src/environments/environment.prod.ts
+++ apps/fe/src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ import {environmentBase} from './environment.base';
 export const environment = {
   ...environmentBase,
   production: true,
-  webServiceUri: 'https://body-data-crud.herokuapp.com/api/v1/'
+  webServiceUri: '/api/v1/'
 };
