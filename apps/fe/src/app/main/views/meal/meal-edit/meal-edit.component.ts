0fce7a8fceff852728a4ecbec2a7bab8dc144685
mtrapass <matteo.trapasso97@gmail.com>
2021-06-25 17:18:58 +0200

    chart update!

--- apps/fe/src/app/main/views/meal/meal-edit/meal-edit.component.ts
+++ apps/fe/src/app/main/views/meal/meal-edit/meal-edit.component.ts
@@ -1,17 +1,19 @@
-import {Component} from '@angular/core';
+import {ChangeDetectionStrategy, Component} from '@angular/core';
 import {closePopUpAction, PopUpBaseComponent} from '@root-store/router-store/pop-up-base.component';
 import {FormArray, FormControl, FormGroup, Validators} from '@angular/forms';
 import {MealStoreActions} from "@root-store/meal-store/index";
 import {Meal, MenuItem} from "@models/vo/meal";
-import {Observable, Subscription} from "rxjs";
+import {BehaviorSubject, Observable} from "rxjs";
 import {FoodStoreSelectors} from "@root-store/food-store/index";
 import {Food} from "@models/vo/food";
+import {map} from "rxjs/operators";
 
 
 @Component({
   selector: 'app-meal-edit',
   templateUrl: './meal-edit.component.html',
-  styleUrls: [`./meal-edit.component.scss`]
+  styleUrls: [`./meal-edit.component.scss`],
+  changeDetection:ChangeDetectionStrategy.OnPush
 })
 export class MealEditComponent extends PopUpBaseComponent<Meal> {
 
@@ -24,47 +26,69 @@ export class MealEditComponent extends PopUpBaseComponent<Meal> {
   time: FormControl;
   foods$: Observable<Food[]>;
   types: string[] = ["colazione", "pranzo", "cena", "spuntino"];
-  data: any;
+  data$: Observable<any>;
   selectedType: any;
   options: any;
-  totFat = 1;
+  chartOptions: any =  {animation: { duration: false }}
+  onChangeFormSubject: BehaviorSubject<any>;
+/*  totFat = 1;
   totProtein = 1;
   totCarbo = 1;
-  totKcal = 0;
-
+  totKcal = 0;*/
 
   setItemPerform(value: Meal): void {
+    this.onChangeFormSubject = new BehaviorSubject<any>(value)
+    this.data$ = this.onChangeFormSubject.asObservable().pipe(
+      map(value1 => {
+        console.log(value1)
+        const fat = (value1.menu.reduce((tot, menu) => tot + +menu.food.Total_fat/100*menu.qty,0))
+        const protein = (value1.menu.reduce((tot, menu) => tot + +menu.food.Total_protein/100*menu.qty,0))
+        const carbo = (value1.menu.reduce((tot, menu) => tot + +menu.food.Available_carbohydrates_MSE/100*menu.qty,0))
+        return this.makeChartData(carbo,protein,fat)
+      })
+    )
+
+
     this.makeFrom();
     this.form.reset(value);
     value.menu.forEach(value1 => {
       this.addMenu(value1)
-    })
-    this.getSumValues(); //calcolo valori
+    })/*
+    this.getSumValues(); //calcolo valori*/
+
+  }
+
+
+
+  makeChartData(fat, protein, aaa): any{
+   return  {
+     options: {
+       animation: {
+         duration: false
+       }
+     },
+      labels: ['Protein', 'Carbohydrates', 'Fat'],
+        datasets: [
+      {
+        data: [fat,protein, aaa],
+        backgroundColor: [
+          "#D32F2F",
+          "#689F38",
+          "#FBC02D"
+        ],
+        hoverBackgroundColor: [
+          "#D32F2F",
+          "#689F38",
+          "#FBC02D"
+        ]
+      }]
+    }
   }
 
   makeFrom(): void {
     this.foods$ = this.store$.select(
       FoodStoreSelectors.selectAll
     );
-
-    this.data = {
-      labels: ['Protein', 'Carbohydrates', 'Fat'],
-      datasets: [
-        {
-          data: [this.totProtein, this.totCarbo, this.totFat],
-          backgroundColor: [
-            "#D32F2F",
-            "#689F38",
-            "#FBC02D"
-          ],
-          hoverBackgroundColor: [
-            "#D32F2F",
-            "#689F38",
-            "#FBC02D"
-          ]
-        }]
-    };
-
 
     this._id = this.fb.control(this._id, Validators.required);
     this.date = this.fb.control({value: '', disabled: true});
@@ -78,6 +102,18 @@ export class MealEditComponent extends PopUpBaseComponent<Meal> {
       time: this.time, // attributo
       menu: this.fb.array([])
     });
+
+
+    this.form.valueChanges.subscribe(
+      value => this.onChangeFormSubject.next(value)
+    )
+
+/*
+      reduce(
+        (prev, next) =>
+          prev + +((next.menu.food.Total_fat / 100) * next.menu.qty), 0)
+    );
+  */
   }
 
 
@@ -143,11 +179,11 @@ export class MealEditComponent extends PopUpBaseComponent<Meal> {
     getSumKcal() {
       this.totKcal = this.menu.value.reduce((prev, next) => prev + +((next.food.Energy_Rec_with_fibre / 100) * next.qty), 0);
     }*/
-  getSumValues() {
+/*  getSumValues() {
     this.totFat = this.menu.value.reduce((prev, next) => prev + +((next.food.Total_fat / 100) * next.qty), 0);
     this.totProtein = this.menu.value.reduce((prev, next) => prev + +((next.food.Total_protein / 100) * next.qty), 0);
     this.totCarbo = this.menu.value.reduce((prev, next) => prev + +((next.food.Available_carbohydrates_MSE / 100) * next.qty), 0);
     this.totKcal = this.menu.value.reduce((prev, next) => prev + +((next.food.Energy_Rec_with_fibre / 100) * next.qty), 0);
-  }
+  }*/
 }
 
